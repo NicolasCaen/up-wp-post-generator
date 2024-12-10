647afaeeff0d791558e8b8713279fb8f0395026d
nicolas.gehin <nico.gehin@gmail.com>
2024-12-10 22:27:24 +0100

    prise en compte des elemen p hn list

--- includes/processors/class-markdown-processor.php
+++ includes/processors/class-markdown-processor.php
@@ -6,7 +6,9 @@ class Markdown_Processor extends Abstract_Content_Processor {
 
     public function process($content) {
         $blocks = parse_blocks($content);
-        return $this->convert_to_markdown($blocks);
+        $markdown = $this->convert_to_markdown($blocks);
+        error_log("markdown final: ".$markdown);
+        return $markdown;
     }
 
     public static function get_type() {
@@ -24,40 +26,49 @@ class Markdown_Processor extends Abstract_Content_Processor {
         
         $markdown = '';
         foreach ($blocks as $block) {
-            // Gestion récursive des blocs imbriqués
-            if (isset($block['innerBlocks']) && !empty($block['innerBlocks'])) {
-                $markdown .= $this->convert_to_markdown($block['innerBlocks']);
-            }
-
-            if (isset($block['innerHTML'])) {
-                if (isset($block['blockName']) && $block['blockName'] === 'core/quote') {
+            // Log pour debug
+            error_log("Type de bloc: " . print_r($block['blockName'], true));
+            error_log("Contenu HTML: " . print_r($block['innerHTML'], true));
+            
+            switch($block['blockName']) {
+                case 'core/heading':
+                    if (preg_match('/<h([1-6])[^>]*>(.*?)<\/h[1-6]>/s', $block['innerHTML'], $matches)) {
+                        $level = $matches[1];
+                        $content = trim($matches[2]);
+                        $markdown .= str_repeat('#', intval($level)) . ' ' . $content . "\n\n";
+                    }
+                    break;
+                    
+                    
+                case 'core/paragraph':
                     if (preg_match('/<p>(.*?)<\/p>/s', $block['innerHTML'], $matches)) {
                         $content = trim($matches[1]);
-                        $markdown .= '> ' . $content . "\n\n";
-                    }
-                }
-                elseif (preg_match('/>([^>]+)<\//', $block['innerHTML'], $matches)) {
-                    $content = $matches[1];
-                    
-                    if (strpos($block['innerHTML'], '<h2') !== false) {
-                        $markdown .= '## ' . $content . "\n\n";
-                    }
-                    elseif (strpos($block['innerHTML'], '<h3') !== false) {
-                        $markdown .= '### ' . $content . "\n\n";
-                    }
-                    elseif (strpos($block['innerHTML'], '<h4') !== false) {
-                        $markdown .= '#### ' . $content . "\n\n";
-                    }
-                    elseif (strpos($block['innerHTML'], '<p') !== false) {
                         $markdown .= $content . "\n\n";
+                        error_log("Paragraphe détecté: " . $content);
                     }
-                    elseif (strpos($block['innerHTML'], '<li') !== false) {
-                        $markdown .= '- ' . $content . "\n";
+                    break;
+                    
+                case 'core/list':
+                    if (isset($block['innerBlocks'])) {
+                        foreach ($block['innerBlocks'] as $item) {
+                            if ($item['blockName'] === 'core/list-item') {
+                                if (preg_match('/<li[^>]*>(.*?)<\/li>/s', $item['innerHTML'], $matches)) {
+                                    $content = trim($matches[1]);
+                                    $markdown .= '- ' . $content . "\n";
+                                }
+                            }
+                        }
+                        $markdown .= "\n";
                     }
-                }
+                    break;
+            }
+
+            if (isset($block['innerBlocks']) && !empty($block['innerBlocks'])) {
+                $inner_markdown = $this->convert_to_markdown($block['innerBlocks']);
+                $markdown .= $inner_markdown;
             }
         }
-        error_log("markdown 1".$markdown);
+        
         return $markdown;
     }
 } 
